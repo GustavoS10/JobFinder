c1d981db38020bf69416b3a40d1ad9a5a499e9fc
Gustavo Sovrani <84320223+GustavoS10@users.noreply.github.com>
2022-06-23 10:43:38 -0300

    Add allowNull : False

--- models/Job.js
+++ models/Job.js
@@ -4,6 +4,9 @@ const db = require('../db/connection');
 const Job = db.define('job', {
   title: {
     type: DataTypes.STRING(100),
+    validate:{
+      allowNull: false,
+    }
   },
   description: {
     type: DataTypes.STRING(),
@@ -12,31 +15,36 @@ const Job = db.define('job', {
         if(value < 50){
           throw new Error("Description require more character!");
         }
-      }
+      },
+      allowNull: false,
     }
   },
   salary: {
     type: DataTypes.STRING(6),
     validate:{
       isInt: true,
+      allowNull: false,
     }
   },
   company: {
     type: DataTypes.STRING,
     validate:{
       not: ["^[!-@-#-$-%-¨-&-*]"],
+      allowNull: false,
     }
   },
   email: {
     type: DataTypes.STRING,
     validate:{
       isEmail: true,
+      allowNull: false,
     }
   },
   new_job: {
     type: DataTypes.INTEGER,
     validate:{
       isInt: true,
+      allowNull: false,
     }
   }
 });
